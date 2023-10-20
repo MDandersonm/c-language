8d335fd53642adba3e256fd648f2a6a20925977b
KimYeongJIn <125858342+MDandersonm@users.noreply.github.com>
2023-10-21 04:42:47 +0900

    commit

--- c-language/01_hello.c
+++ c-language/01_hello.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-//#include는 헤더 파일을 포함하는 문법이며 printf 함수를 사용하려면 stdio.h 헤더 파일이 필요합니다
+//#include는 헤더 파일을 포함하는 문법이며 printf 함수를 사용하려면 stdio.h 헤더 파일이 필요합니다!
 int main()
 {
     printf("Hello, world!\n");
