531c8eff9e7f0497d8cbf7c467718b5ef56f37bd
KimYeongJIn <1215kimyj@gmail.com>
2023-12-08 10:09:39 +0900

    커밋 테스트1

---
+++ c-language1/c-language1/01_print.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main1() {
+	// 문자열 출력
+	printf("안녕하세요, C 언어입니다.\n");
+
+	// 정수 출력
+	int a = 10;
+	printf("a의 값은 %d입니다.\n", a);
+
+	// 실수 출력
+	double b = 3.14159;
+	printf("b의 값은 %.2f입니다.\n", b);
+
+	// 문자 출력
+	char c = 'A';
+	printf("c의 값은 %c입니다.\n", c);
+
+	return 0;
+}
