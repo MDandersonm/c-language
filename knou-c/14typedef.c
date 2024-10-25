11676386f3680257ce04af1b5b2180bc89c36b8b
KimYeongJIn <125858342+MDandersonm@users.noreply.github.com>
2024-10-25 18:06:47 +0900

    knou-c\14typedef.c

---
+++ knou-c/14typedef.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+
+//typedef 이미 존재하는 자료형에 새로운 이름을 붙이기 위한 키워드
+//typedef 기존자료형 새로운 자료형이름;
+
+
+typedef unsigned int BYTE;
+
+typedef struct data {
+	int x;
+	int y;
+
+} DATA;
+
+struct data2{
+	int x;
+	int y;
+
+};
+
+typedef struct data DATA2;
+
+void main140() {
+	BYTE val;
+
+
+
+}
