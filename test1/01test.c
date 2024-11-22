412d11bd84a194d0d2e5beaa53022265a81a614f
KimYeongJIn <1215kimyj@gmail.com>
2024-11-22 14:47:16 +0900

    test

---
+++ test1/01test.c
@@ -0,0 +1,15 @@
+typedef struct ListNode {   // 단순 연결 리스트의 노드 구조 정의
+
+    int data;
+    listNode* link;
+} listNode;
+
+typedef struct {       // 리스트의 헤드(first) 노드 구조 정의
+
+    listNode* head;
+
+} linkedList_h;
+
+int main() {
+    return 0;
+}
